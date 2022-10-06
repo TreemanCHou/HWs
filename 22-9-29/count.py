c420685942d9f3f6b3c893484d3cf24ba00e86cb
TreemanChou <574747067@qq.com>
2022-10-06 16:57:16 +0800

    Viterbi .

--- 22-9-29/count.py
+++ 22-9-29/count.py
@@ -1,3 +1,6 @@
+import sys
+from tqdm import tqdm
+
 if __name__ == '__main__':
     filestream = open('train.001', 'r', encoding='GBK')
     freq = {}
@@ -7,23 +10,28 @@ if __name__ == '__main__':
                 freq[single] = 0
             if single in freq:
                 freq[single] += 1
-    print(len(freq))
-
+    total = len(freq)
     rsltfile = open('rslt.txt', 'w', encoding='GBK')
 
-    for char1 in freq:
-        for char2 in freq:
-            count = 0
-            doublec = str(char1)+str(char2)
-            # print(doublec)
-            # 在这里修正一下指针位置试试
-            filestream.seek(0, 0)
-            for line in filestream:
-                count += line.count(doublec)
-            P = count / freq[char1]
-            if P != 0:
-                # print('writing :'+str(char1) + ' ' + str(char2) + ' ' + str(P))
-                rsltfile.write(str(char1) + ' ' + str(char2) + ' ' + str(P) + '\n')
+    mark_str = '\n\t\r `1234567890-=~!@#$%^&*()_+,./;\'[]\\<>?:\"{}|～！…￥（）——`～{}|·「」、；‘’“”，。/《》？'
+    combine_dict = {}
+    print(total)
+    filestream.seek(0)
+    print('READING&COUNTING: ')
+    for line in tqdm(filestream):
+        for i in range(len(line)-1):
+            # print('COMBINATION:'+str(line[i]) + ' AND ' + str(line[i+1]))
+            if line[i] in mark_str or line[i+1] in mark_str:
+                continue
+            comb = str(line[i]) + str(line[i+1])
+            if comb not in combine_dict:
+                combine_dict[comb] = 1
+            else:
+                combine_dict[comb] += 1
+    print('WRITING :', end='')
+    for item in tqdm(list(combine_dict)):
+        print(item+' : '+str(f"{combine_dict[item]/freq[item[0]]:.20f}"))
+        rsltfile.write(str(item[0]) + ' ' + str(item[1]) + ' ' + str(f"{combine_dict[item]/freq[item[0]]:.20f}") + '\r\n')
 
     rsltfile.close()
     filestream.close()
